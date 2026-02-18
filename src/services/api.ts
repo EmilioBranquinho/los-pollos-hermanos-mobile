2d93f25a610ca39a14036db0c24a56995992d13d
Emílio Marcelino Honório Branquinho <emiliomarcelino400@gmail.com>
2026-02-18 20:39:47 +0000

    feat: changed the api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://192.168.43.208:9000",
+    // baseURL: "http://192.168.43.208:9000",
+    baseURL: "https://api-los-pollos-hermanos.onrender.com"
 });
 
 export { api }
